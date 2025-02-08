188da95d6629e6fc3159df61cc5d1bb7c84b307a
TuTu <tutu@jyc.cable.rcn.com>
2025-02-07 20:30:24 -0500

    Upload new

--- class_and_object/src/class_and_object/Framework.java
+++ class_and_object/src/class_and_object/Framework.java
@@ -1,1 +1,22 @@
 import java.util.ArrayList;//动态数组
+import java.util.LinkedList;//双向链表, queue
+import java.util.PriorityQueue; //stack
+
+import java.util.Stack; // stack
+import java.util.Vector;//动态数组
+
+import java.util.HashSet;// Hash table
+import java.util.LinkedHashMap;//保留插入顺序，有顺序
+import java.util.TreeMap;
+
+
+
+public class Framework {
+    public static void main(String[] args){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Bob");
+        arrayList.add("Pet");
+        System.out.println(arrayList);
+
+    }
+}
